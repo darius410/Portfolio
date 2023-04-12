8c27cc545d529c97677a34756dfb05b3c8356711
Chaos <tinywonk@yahoo.com>
2023-04-11 22:16:17 -0400

    sm and lg screens are styled

--- App/src/components/js/About.tsx
+++ App/src/components/js/About.tsx
@@ -4,14 +4,11 @@ import { AiFillTwitterCircle } from 'react-icons/ai'
 import { Icon } from '@iconify/react';
 import brandGithubFilled from '@iconify/icons-tabler/brand-github-filled';
 import { MdFacebook } from 'react-icons/md'
+import SocialLinks from './SocialLinks';
 
 function About() {
 
-  const SocialIcon = ({icon}) => (
-    <button className="SocialIcon">
-        {icon}
-    </button>
-)
+
 
     return (
 
@@ -20,20 +17,18 @@ function About() {
 
      {/* CONTAINER */}
     <div className="matrixBack justify-center h-full bg-black text-white" >
-    <h2 className="text-5xl py-5 ml-2 lg:text-6xl"><span className="bg-white text-black">Who </span>Am I ?</h2>
+    <h2 className="text-5xl py-5 ml-2 lg:text-8xl font-primary lg:ml-20"><span className=" text-black">Who</span> Am I ?</h2>
       <div className="flex flex-col">
-          <div className="w-4/5 justify-center m-auto  lg:w-1/2 lg:text-2xl ">
-          <h2 className="w-full h-full  text-2xl text-cyan-50  lg:text-4xl lg:w lg:my-7 " >My Name is Darius Hansley And I Am Here To code   </h2>
-            <p className="leading-6">Labore pariatur laboris et consequat in dolor adipisicing sit nisi do.
-              Ipsum mollit incididunt est ea reprehenderit adipisicing tempor occaecat officia proident do ad Lorem.Occaecat quis adipisicing ut quis.</p>
+          <div className="w-4/5 justify-center m-auto  lg:w-1/2 lg:text-3xl">
+          <h2 className="font-primary my-7 w-full h-full  text-3xl text-cyan-50  lg:text-6xl text-center  " >My Name is <span className="text-black h-3"> Darius Hansley</span> And I Am Here To code   </h2>
+            <p className="text-2xl mb-6 leading-6 font-secondary font-light tracking-wider ">Labore pariatur laboris et consequat in dolor adipisicing sit nisi do.
+              Ipsum mollit incididunt est ea reprehenderit adipisicing tempor occaecat officia proident do ad Lorem.Occaecat quis adipisicing ut quis.Pariatur aliqua tempor incididunt enim id velit do eiusmod irure consequat mollit duis mollit.In id sunt non sunt commodo dolor non laborum nostrud.Amet velit esse pariatur ad laborum in est et velit.</p>
+
+         
 
               
-            <ul className=" flex flex-row p-6 justify-center m-auto  socialLinks">
-              
-              <li>Facebook</li>
-      
-                    
-            </ul>
+           <SocialLinks/>
+
           </div>
 
           <div className="flex m-auto justify-center lg:w-4/6">
@@ -42,16 +37,16 @@ function About() {
             
           </div>
 
-            <h1 className=" justify-center text-center text-4xl lg:text-6xl lg:mb-12">My Blogs</h1>
-            <ol className="list-decimal flex flex-col px-6 pb-10 justify-center m-auto border-b-2 border-solid w-4/5  lg:flex-row ">
+            <h1 className=" ml-2 lg:text-8xl font-primary lg:ml-20 justify-center mb-4 mt-[100px] text-6xl lg:mt-[200px]">My <span className="text-black h-3">Blogs</span></h1>
+            <ol className="font-secondary flex flex-col lg:flex-wrap gap-1 px-2 pb-10 lg:pb-10 justify-center m-auto border-b-2 border-solid w-4/5 lg:h-[300px] lg:gap-2 ">
               
-              <li>Love<p>Esse aute nostrud nulla ea.Duis pariatur et eiusmod sint voluptate consectetur magna fugiat nostrud mollit culpa dolore nostrud cupidatat.</p></li>
-  <li>Love<p>Esse aute nostrud nulla ea.Sint aute mollit dolore laborum commodo.</p></li>
-     <li>Love<p>Esse aute nostrud nulla ea.Pariatur Lorem labore non duis proident aute et adipisicing.</p></li>
-  <li>Love<p>Esse aute nostrud nulla uiu kuygyivyv jf tudysy yfuv yfuv  tyvuvgvt uv uuttfu ea.</p></li>
-     <li>Love<p>Esse aute nostrud nulla ea.</p></li>
-  <li>Love<p>Esse aute nostrud nulla ea.Lorem velit mollit commodo reprehenderit veniam quis pariatur laboris nulla.</p></li>
-
+              <li className="bg-white text-black lg:w-1/2 lg:h-20 text-3xl pl-3">Mastering Javascript Before 30<p className="text-base">Esse aute nostrud nulla ea.Duis pariatur et eiusmod sint voluptate consectetur magna fugiat nostrud mollit culpa dolore nostrud cupidatat.</p></li>
+              <li className="bg-white text-black lg:w-1/2 lg:h-20 text-3xl pl-3">Why Learning Code On Youtube Is Harder Then It Should Be<p className="text-base">Esse aute nostrud nulla ea.Duis pariatur et eiusmod sint voluptate consectetur magna fugiat nostrud mollit culpa dolore nostrud cupidatat.</p></li>
+              <li className="bg-white text-black lg:w-1/2 lg:h-20 text-3xl pl-3">Things I hate About Your Video Game Trailer<p className="text-base">Esse aute nostrud nulla ea.Duis pariatur et eiusmod sint voluptate consectetur magna fugiat nostrud mollit culpa dolore nostrud cupidatat.</p></li>
+              <li className="bg-white text-black lg:w-1/2 lg:h-20 text-3xl pl-3">The Future Of Video Games Are Free To Play RogueSoul Lites<p className="text-base">Esse aute nostrud nulla ea.Duis pariatur et eiusmod sint voluptate consectetur magna fugiat nostrud mollit culpa dolore nostrud cupidatat.</p></li>
+              <li className="bg-white text-black lg:w-1/2 lg:h-20 text-3xl pl-3">Should You Invest In Video game Companies?<p className="text-base">Esse aute nostrud nulla ea.Duis pariatur et eiusmod sint voluptate consectetur magna fugiat nostrud mollit culpa dolore nostrud cupidatat.</p></li>
+              <li className="bg-white text-black lg:w-1/2 lg:h-20 text-3xl pl-3">Am I Afraid Of AI Taking Jobs ?<p className="text-base">Esse aute nostrud nulla ea.Duis pariatur et eiusmod sint voluptate consectetur magna fugiat nostrud mollit culpa dolore nostrud cupidatat.</p></li>
+           
   
             </ol>
         
@@ -62,6 +57,8 @@ function About() {
 
    
   }
+
+
   
   export default About
   
