08a181752bfd535e6805df44101ff4f11810a1a3
Chaos <tinywonk@yahoo.com>
2023-04-30 21:17:15 -0400

    Fixed an Issue with logo on Lg screens

--- App/src/components/js/Footer.tsx
+++ App/src/components/js/Footer.tsx
@@ -10,9 +10,9 @@ function Footer() {
 
         <Divider />
 
-        <div className="flex flex-col justify-center m-auto text-center text-cyan-50 bg-black text-3xl  w-full h-[150px] lg:flex-row lg:justify-around lg:h-[100px] lg:items-center " >
+        <div className="flex flex-col justify-center m-auto text-center text-cyan-50 bg-black text-3xl  w-full h-[150px]  lg:px-20 lg:flex-row lg:justify-between lg:h-[100px] lg:items-center" >
      
-                  <img src="./src/img/Untitled-2.png" className="w-3/5 self-center"/>
+                  <img src="./src/img/Untitled-2.png" className="w-3/5 self-center lg:w-1/4"/>
 
                 
 
--- App/src/components/js/Hero.tsx
+++ App/src/components/js/Hero.tsx
@@ -17,10 +17,10 @@ function Hero() {
             </button>
       </div>
         
-        <div className="flex flex-col mb-12 lg:w-1/2">  
-            <img className=" lg:w-3/5 mx-10 md:mx-32 lg:mr-20 lg:mt-2"  src="./src/img/Darius.jpg" />
+        <div className=" bg-zinc-900 flex flex-row mb-12  lg:w-1/2">  
+            <img className="w-3/5 h-[300px]  lg:w-3/5 md:mx-32 lg:mr-20 lg:mt-2"  src="./src/img/Darius.jpg" />
 
-            <p className="font-primary text-center text-2xl text-cyan-50 mx-16 mb-12 mt-2 md:mx-32 lg:hidden" >My Objective is to make sites and applications that look amazing and perform at their best.
+            <p className="font-primary stroke sideways text-[90px] h-[200px] w-[700px] text-black" >WHO AM I
             </p>
             <button id="" className="m-auto font-primary text-cyan-50 border-2 w-1/2 text-2xl lg:hidden">"See My Work" </button>
         </div>
