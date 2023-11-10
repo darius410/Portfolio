d66f34167253d5a355f8a9348aa01da2ae5e1d78
Chaos <tinywonk@yahoo.com>
2023-11-10 16:17:58 -0500

    Added git ignore file

--- App/vite.config.ts
+++ App/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/", 
-})
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+    assetsDir: 'assets', // Specify the assets directory within the output directory
+    rollupOptions: {
+      input: {
+        main: './index.html', // Adjust the entry point as needed
+      },
+    },
+  },
+});
