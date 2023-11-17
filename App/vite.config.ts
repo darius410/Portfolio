3b0b5d91364a108402e423423b70fafee17561a0
Chaos <tinywonk@yahoo.com>
2023-11-17 18:35:52 -0500

    Trigger Build

--- App/vite.config.ts
+++ App/vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portfolio/", 
   plugins: [react()],
+  
   build: {
     outDir: 'dist', // Specify the output directory for the build
     assetsDir: 'assets', // Specify the assets directory within the output directory
