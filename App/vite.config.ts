1281c4f5ef41e983dbc01bf89ce3381f637e02bf
Chaos <tinywonk@yahoo.com>
2023-11-17 19:36:11 -0500

    Trigger Build

--- App/vite.config.ts
+++ App/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "index.html", 
+  base: "App", 
   plugins: [react()],
   
   build: {
